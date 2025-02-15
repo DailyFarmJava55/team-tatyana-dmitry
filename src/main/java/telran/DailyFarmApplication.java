c6fdffb2cbce896214e58cbb2c539295b8bb3d27
raevsky66 <raevsky66@gmail.com>
2025-02-15 22:35:49 +0200

    Initial comment

---
+++ src/main/java/telran/DailyFarmApplication.java
@@ -0,0 +1,13 @@
+package telran;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailyFarmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DailyFarmApplication.class, args);
+	}
+
+}
