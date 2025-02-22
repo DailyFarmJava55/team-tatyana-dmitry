c1dcc15c7038dd67eab00f8601ee5ec94ce1b5f9
raevsky66 <raevsky66@gmail.com>
2025-02-22 16:38:24 +0200

    del goods add auth

--- src/main/java/telran/configuration/ServiceConfiguration.java
+++ src/main/java/telran/configuration/ServiceConfiguration.java
@@ -5,16 +5,28 @@ import org.modelmapper.config.Configuration.AccessLevel;
 import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import telran.dailyFarm.accounting.dto.farmer.FarmerDto;
+import telran.dailyFarm.accounting.model.farmer.Farmer;
+
 
 @Configuration
 public class ServiceConfiguration {
-
 	@Bean
 	ModelMapper getModelMapper() {
 		ModelMapper modelMapper = new ModelMapper();
-		modelMapper.getConfiguration().setFieldMatchingEnabled(true).setFieldAccessLevel(AccessLevel.PRIVATE)
-				.setMatchingStrategy(MatchingStrategies.STRICT);
-		return modelMapper;
-	}
+		modelMapper.getConfiguration()
+									.setFieldMatchingEnabled(true)
+									.setFieldAccessLevel(AccessLevel.PRIVATE)
+									.setMatchingStrategy(MatchingStrategies.STRICT);
 
+		return modelMapper;
+		}
+		@Bean
+		PasswordEncoder getPasswordEncoder() {
+			return new BCryptPasswordEncoder();
+		
+	}
 }
