7ad748de2eef986835a8f1a4a91c47a3e26ad376
TatyanaNikitenko <tatiananikitenko8@gmail.com>
2025-03-12 14:28:49 +0200

    update
    upd model: User, Farmer and SurpriseBox add fields

--- src/main/java/telran/surpriseBox/model/SurpriseBox.java
+++ src/main/java/telran/surpriseBox/model/SurpriseBox.java
@@ -1,6 +1,5 @@
 package telran.surpriseBox.model;
 
-
 import java.util.List;
 import java.util.UUID;
 
@@ -14,28 +13,39 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
+import telran.auth.account.model.Farmer;
+import telran.auth.account.model.User;
 
 @Data
 @Entity
 @AllArgsConstructor
 @NoArgsConstructor
 public class SurpriseBox {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private UUID id;
 
-    @ElementCollection(targetClass = Category.class)
-    @Enumerated(EnumType.STRING)
-    @CollectionTable(name = "surprise_box_categories", joinColumns = @JoinColumn(name = "surprise_box_id"))
-    @Column(name = "category")
-    private List<Category> categories;
-    private UUID farmerId;
-    private BoxSize boxSize;
-    private String description;
-    private double price;
-    private int quantity;
+	@ElementCollection(targetClass = Category.class)
+	@Enumerated(EnumType.STRING)
+	@CollectionTable(name = "surprise_box_categories", joinColumns = @JoinColumn(name = "surprise_box_id"))
+	@Column(name = "category")
+	private List<Category> categories;
+	private String description;
+
+	@ManyToOne
+	@JoinColumn(name = "farmer_id", nullable = false)
+	private Farmer farmer;
+
+	@ManyToOne
+	@JoinColumn(name = "customer_id")
+	private User customer;
+
+	private BoxSize boxSize;
+
+	private double price;
+	private int quantity;
 }
